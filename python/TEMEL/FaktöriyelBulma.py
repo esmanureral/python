fad5e19279af79edb544e4e78ec3e619c36dd7dc
Esmanur <esmanur2eral@gmail.com>
2024-06-24 23:14:15 +0300

    Create FaktöriyelBulma.py

---
+++ python/TEMEL/FaktöriyelBulma.py
@@ -0,0 +1,18 @@
+print(""" SAYININ FAKTÖRİYELİNİ BULMA 
+      ÇIKMAK İÇİN Q YA BASINIZ""")
+
+while True:
+    sayi=input("sayi gir.")
+    if(sayi=="q"):
+        print("çıkış yapılıyor..")
+        break
+    
+    else:
+        sayi=int(sayi)
+        faktöriyel=1
+        
+        for i in range(2,sayi+1):
+            faktöriyel*=i
+        print("faktöriye:",faktöriyel)
+                
+    
