b737334bae5914144ed9548f71a30fac47a50d2c
Esmanur <esmanur2eral@gmail.com>
2024-07-18 22:12:29 +0300

    101

---
+++ python/TEMEL/konumsu/sayı/karakter.py
@@ -0,0 +1,31 @@
+#abs:mutlak değer
+
+print(abs(-4))
+
+#round:yuvarlama işlemi
+
+print(round(4.9))
+
+#pow:üs alma
+
+print(pow(3,4))
+
+#upper():stringin tüm karakterlerini büyük harfe çevirir.
+#lower():stringin tüm karakterlerini küçük harfe çevirir.
+
+print("python".upper())
+print("PytHon".lower())
+
+
+#replace(x,y):stringteki x değerini y ile değiştirir
+
+print("Kunduz".replace("duz","zun"))
+
+#startswith(x):x ile başlıyorsa True,başlamıyorsa False
+#endswith(x):x ile bitiyorsa True,bitmiyorsa False
+
+#split(a):verilen a değerine göre string parçalara ayrılarak her bir parça listeye atılır.
+
+#strip(x):başında ve sonunda bulunan x değerini siler
+#lstrip(x):sadece başında bulunan x değerini siler.
+#rstrip(x):sadece sonunda bulunan x değerini siler.
